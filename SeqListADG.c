76b20ad71d69efafb246ef376371f58b05ff3106
xiaocaijie <51046504+xiaocaijie@users.noreply.github.com>
2020-11-13 21:51:07 +0800

    线性表增删查
    添加了菜单

--- SeqListADG.c
+++ SeqListADG.c
@@ -6,21 +6,84 @@ int elem[MAXSIZE];
 int last;
 }SeqList, *LinkList;
 
-
-
+SeqList seqList;
+LinkList p = &seqList;
 void main() {
 void dispalyList(SeqList List); //打印elem[]
 void innit (LinkList p ); //初始化elem[]
 int InsList(LinkList p, int i , int e );//插入
 int DelList (LinkList p,int i,int *e);//删除
 int Locate (SeqList L, int e);//查询
-SeqList seqList;
-int i,e;
-LinkList p = &seqList;
-p->last = 4; //共有5条数据
+//菜单
+void Search(); 
+void Delect ();
+void Insert();
+int flag = 1,select;
 
-innit(p);
-dispalyList(seqList);
+while (flag !=0) {
+printf ("输入 0退出 1初始化  2插入 3删除 4查询 5输出线性表 :");
+scanf("%d",&select);
+switch (select) {
+case 0 : flag = 0; break;
+case 1 : innit(p); break;
+case 2 : Insert(); break;
+case 3 : Delect (); break;
+case 4 : Search(); break;
+case 5 : dispalyList(seqList);break;
+}
+
+} 
+
+}
+
+
+void Search() {
+int flag= 1,e,i; 
+while (flag) {
+//查询
+printf("请输入要查询的数据：");
+scanf("%d",&e);
+i = Locate ( seqList,e);
+if(i!= -1) {
+	printf("查询成功 %d位置为%d\n",e,i);
+
+}else {
+	printf("没有找到");
+}
+
+printf("继续查询请输入 1  退出输入0:");
+scanf("%d",&flag);
+
+}
+}
+
+//删除
+void Delect () {
+int flag = 1,i,e; 
+while (flag) {
+//删除
+printf("请输入要删除的位置：");
+scanf("%d",&i);
+if (DelList ( p,i,&e)){
+	printf("删除成功 删除元素为:%d\n",e);
+	dispalyList(seqList);
+}else {
+	printf("删除失败");
+
+}
+printf("继续删除请输入 1  退出输入0:");
+scanf("%d",&flag);
+
+
+}
+}
+
+
+
+//插入
+void Insert() {
+int flag=1,e,i;
+while (flag) {
 
 //插入
 printf("请输入要插入的数据:");
@@ -36,44 +99,27 @@ if(InsList( p,i,e )) {
 	printf("插入失败");
 }
 
-//删除
-printf("请输入要删除的位置：");
-scanf("%d",&i);
-if (DelList ( p,i,&e)){
-	printf("删除成功 删除元素为:%d\n",e);
-	dispalyList(seqList);
-}else {
-	printf("删除失败");
+printf("继续插入请输入 1  退出输入0:");
+scanf("%d",&flag);
+
+
+} 
 
 }
-
-//查询
-printf("请输入要查询的数据：");
-scanf("%d",&e);
-i = Locate ( seqList,e);
-if(i!= -1) {
-	printf("查询成功 %d位置为%d\n",e,i);
-
-}else {
-	printf("没有找到");
-}
-
-
-}
-
-
-
-
 
 
 
 void innit (LinkList p )  {
-int i;
+int i,n;
+printf("请输入数据的总数:");
+scanf("%d",&n);
+p->last = n-1;
 printf("初始化数据\n");
 for (i = 0; i <= p->last ; i++) {
-printf("请输入第%d个元素",i+1);
+printf("请输入第%d个元素:",i+1);
 scanf("%d",&p->elem[i]);
 }
+
 }
 
 
